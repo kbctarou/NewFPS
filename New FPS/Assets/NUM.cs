2118ae74f5812b3a810d4dc70a31e2fbb9723f35
gokiburi00 <kbc15b10@stu.kawahara.ac.jp>
2016-11-25 11:36:45 +0900

    score

---
+++ New FPS/Assets/NUM.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NUM : MonoBehaviour {
+    public Text numText;
+    private int num = 0;
+	// Use this for initialization
+	void Start () {
+
+        numText.text = "0";
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+}
