9d9b64461fc0fbd2b8c66858ba2614d0ea81be9f
sugahara <a>
2017-02-09 17:41:15 +0900

    くりあ、げーむーばー ｓくりぷと

---
+++ New FPS/Assets/Team/Script/ScriptScale.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptScale : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void Update()
+    {
+        Vector3 Scale = transform.localScale;
+        if (Scale.y <= 10.0f)
+        {
+            Scale.y += 0.1f;
+            transform.localScale = Scale;
+        }
+
+    }
+}
