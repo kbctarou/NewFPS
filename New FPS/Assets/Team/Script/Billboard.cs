8daecaa9051ff680936267bb17d08d5e60496179
ShoOkawa <a>
2017-03-02 11:12:29 +0900

    提出用に作品ドキュメントなどを追加

---
+++ New FPS/Assets/Team/Script/Billboard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+
+    private GameObject m_TargetCamera = null;
+    public GameObject TargetCamera
+    {
+        set { m_TargetCamera = value; }
+    }
+
+    // Use this for initialization
+    void Start () {        
+        //Vector3 target = m_TargetCamera.transform.position;
+        //transform.LookAt(target);
+        //transform.localRotation *= Quaternion.AngleAxis(90.0f, new Vector3(1.0f, 0.0f, 0.0f));
+        //m_IsRotate = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion rota = m_TargetCamera.transform.rotation;
+        Quaternion rotaInv = /*Quaternion.Inverse(*/rota/*)*/;
+        transform.rotation = rotaInv;
+        transform.rotation *= Quaternion.AngleAxis(-90.0f, new Vector3(1.0f, 0.0f, 0.0f));
+    }
+}
